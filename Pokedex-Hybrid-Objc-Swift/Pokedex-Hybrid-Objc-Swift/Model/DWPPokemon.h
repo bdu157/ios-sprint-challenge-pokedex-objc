591e0cf64d49b6fe1ecd5379de05a3aff640460f
Dongwoo Pae <dongwoopae@gmail.com>
2019-11-23 13:01:26 +0900

    added DWPPokemon model

--- Pokedex-Hybrid-Objc-Swift/Pokedex-Hybrid-Objc-Swift/Model/DWPPokemon.h
+++ Pokedex-Hybrid-Objc-Swift/Pokedex-Hybrid-Objc-Swift/Model/DWPPokemon.h
@@ -8,10 +8,17 @@
 
 #import <Foundation/Foundation.h>
 
-NS_ASSUME_NONNULL_BEGIN
-
+NS_SWIFT_NAME(Pokemon)
 @interface DWPPokemon : NSObject
+
+@property (nonatomic, copy, readonly, nonnull) NSString *name;
+@property (nonatomic, copy, nullable) NSString *identifier;
+@property (nonatomic, copy, nullable) NSArray *abilities;
+@property (nonatomic, copy, nullable) NSString *sprite;
+
+-(nonnull instancetype)initWithName:(nonnull NSString *)name identifier:(nullable NSString *)identifier abilities:(nullable NSArray *)abilities sprite:(nullable NSString *)sprite;
+
+-(nullable instancetype)initWithDictionary:(nonnull NSDictionary *)dictionary;
 
 @end
 
-NS_ASSUME_NONNULL_END
--- Pokedex-Hybrid-Objc-Swift/Pokedex-Hybrid-Objc-Swift/Model/DWPPokemon.m
+++ Pokedex-Hybrid-Objc-Swift/Pokedex-Hybrid-Objc-Swift/Model/DWPPokemon.m
@@ -10,4 +10,23 @@
 
 @implementation DWPPokemon
 
+- (instancetype)initWithName:(NSString *)name identifier:(NSString *)identifier abilities:(NSArray *)abilities sprite:(NSString *)sprite
+{
+    self = [super init];
+    if (self) {
+        _name = name.copy;
+        _identifier = identifier.copy;
+        _abilities = abilities.copy;
+        _sprite = sprite.copy;
+    }
+    return self;
+}
+
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary
+{
+    
+    NSString *name = dictionary[@"name"];
+    return [self initWithName:name identifier:NULL abilities:NULL sprite:NULL];
+}
+
 @end
